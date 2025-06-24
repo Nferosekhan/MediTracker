1e4e6920f23019b3aa3092bca64a80ea08ade753
N.Ferose Khan <148988052+Nferosekhan@users.noreply.github.com>
2025-06-24 11:58:02 +0530

    Delete front-end/src/config.js

--- front-end/src/config.js
+++
@@ -1,5 +0,0 @@
-const isLocalhost = window.location.hostname === "localhost";
-
-export const BASE_URL = isLocalhost
-  ? "http://localhost/meditracksystem/api"
-  : "http://dctcontrichy.com/meditrack/api";
